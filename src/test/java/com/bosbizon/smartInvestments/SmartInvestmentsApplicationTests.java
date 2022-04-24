0ed2452fa967b5f5cfe1100e2dd5cf330a551bcd
Kushdev Singh Luhach <kushdev@gmail.com>
2022-04-23 20:15:45 -0400

    first commit

---
+++ src/test/java/com/bosbizon/smartInvestments/SmartInvestmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosbizon.smartInvestments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInvestmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
